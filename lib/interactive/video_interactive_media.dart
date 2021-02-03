6880e9aee6fc0d50f16a8fcacd3283dd25e96ff2
Robert Virkus <robert.virkus@enough.de>
2021-02-03 19:03:39 +0100

    use better video player controls with Chewie

--- lib/interactive/video_interactive_media.dart
+++ lib/interactive/video_interactive_media.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:path_provider/path_provider.dart' as pp;
@@ -14,7 +15,8 @@ class VideoInteractiveMedia extends StatefulWidget {
 }
 
 class _VideoInteractiveMediaState extends State<VideoInteractiveMedia> {
-  VideoPlayerController _controller;
+  VideoPlayerController _videoController;
+  ChewieController _chewieController;
   Future<VideoPlayerController> _loader;
 
   @override
@@ -24,8 +26,9 @@ class _VideoInteractiveMediaState extends State<VideoInteractiveMedia> {
   }
 
   void dispose() {
-    if (_controller != null) {
-      _controller.dispose();
+    if (_videoController != null) {
+      _videoController.dispose();
+      _chewieController.dispose();
     }
     super.dispose();
   }
@@ -37,18 +40,35 @@ class _VideoInteractiveMediaState extends State<VideoInteractiveMedia> {
       final directory = await pp.getTemporaryDirectory();
       final file = File('${directory.path}/${provider.name}');
       await file.writeAsBytes(provider.data, flush: true);
-      _controller = VideoPlayerController.file(file);
+      _videoController = VideoPlayerController.file(file);
     } else if (provider is UrlMediaProvider) {
-      _controller = VideoPlayerController.network(provider.url);
+      _videoController = VideoPlayerController.network(provider.url);
     } else if (provider is AssetMediaProvider) {
-      _controller = VideoPlayerController.asset(provider.assetName);
+      _videoController = VideoPlayerController.asset(provider.assetName);
     } else {
       throw StateError('Unsupported media provider $provider');
     }
-    await _controller.initialize();
-    await _controller.play();
-    _controller.addListener(() => setState(() {}));
-    return _controller;
+    await _videoController.initialize();
+    _chewieController = ChewieController(
+      videoPlayerController: _videoController,
+      autoPlay: true,
+      looping: false,
+      // Try playing around with some of these other options:
+
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
+    //_videoController.addListener(() => setState(() {}));
+    return _videoController;
   }
 
   @override
@@ -75,101 +95,10 @@ class _VideoInteractiveMediaState extends State<VideoInteractiveMedia> {
     return Container(
       color: Colors.black,
       child: Center(
-        child: AspectRatio(
-          aspectRatio: _controller.value.aspectRatio,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              VideoPlayer(_controller),
-              _ControlsOverlay(controller: _controller),
-              VideoProgressIndicator(_controller, allowScrubbing: true),
-            ],
-          ),
+        child: Chewie(
+          controller: _chewieController,
         ),
       ),
     );
-  }
-}
-
-class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({Key key, this.controller}) : super(key: key);
-
-  static const _examplePlaybackRates = [
-    0.25,
-    0.5,
-    1.0,
-    1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
-  ];
-
-  final VideoPlayerController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AnimatedSwitcher(
-          duration: Duration(milliseconds: 50),
-          reverseDuration: Duration(milliseconds: 200),
-          child: controller.value.isPlaying
-              ? SizedBox.shrink()
-              : Container(
-                  color: Colors.black26,
-                  child: Center(
-                    child: Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
-                      size: 100.0,
-                    ),
-                  ),
-                ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.value.isPlaying ? controller.pause() : play();
-          },
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (context) {
-              return [
-                for (final speed in _examplePlaybackRates)
-                  PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.playbackSpeed}x'),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  void play() async {
-    final position = controller.value.position;
-    if (position == controller.value.duration) {
-      await controller.seekTo(Duration.zero);
-    }
-    await controller.play();
   }
 }
